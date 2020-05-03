d299158edf5adb05e009393d3d3d734ffb5da1fe
Sergio <sergiocelemin1998@gmail.com>
2020-05-03 16:33:36 -0500

    group and student api finished

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,48 @@
 class GroupsController < ApplicationController
+
+    before_action :set_teacher
+    before_action :set_group, only: [:show, :update, :destroy]
+  
+    # GET /teachers/:teacher_id/groups
+    def index
+      json_response(@teacher.groups)
+    end
+  
+    # GET /teachers/:teacher_id/groups:id
+    def show
+      json_response(@groups)
+    end
+  
+    # POST /teachers/:teacher_id/groups
+    def create
+      @groups.groups.create!(group_params)
+      json_response(@groups, :created)
+    end
+  
+    # PUT  /teachers/:teacher_id/groups:id
+    def update
+      @group.update(group_params)
+      head :no_content
+    end
+  
+    # DELETE  /teachers/:teacher_id/groups:id
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def group_params
+      params.permit(:code, :name)
+    end
+  
+    def set_teacher
+      @teacher = Teacher.find(params[:teacher_id])
+    end
+  
+    def set_group
+      @group = @teacher.groups.find_by!(id: params[:id]) if @group
+    end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,47 @@
 class StudentsController < ApplicationController
+
+  before_action :set_group
+  before_action :set_student, only: [:show, :update, :destroy]
+
+  # GET /teachers/:teacher_id/groups/group:id/students
+  def index
+    json_response(@group.students)
+  end
+
+  # GET/teachers/:teacher_id/groups/group:id/students:id
+  def show
+    json_response(@students)
+  end
+
+  # POST /teachers/:teacher_id/groups
+  def create
+    @student.students.create!(student_params)
+    json_response(@student, :created)
+  end
+
+  def update
+    @student.update(student_params)
+    head :no_content
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    head :no_content
+  end
+
+  private
+
+  def student_params
+    params.permit(:name, :age)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_student
+    @student = @group.find_by!(id: params[:id]) if @student
+  end
+     
 end
